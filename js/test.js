b04e09b12a4aa44622174dac17226b2846918389
中坂倖大 <nakasakakota@nakasakasaiwaidainoMacBook-Pro.local>
2021-07-08 03:40:37 +0900

    first commit

---
+++ js/test.js
@@ -0,0 +1,18 @@
+$(function () {
+    $(".slider").slick({
+      arrows: false,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      centerMode: true,
+      centerPadding: "30px",
+      dots: true,
+      // fade: true,
+      slidesToShow: 3,
+      speed: 1000,
+    });
+  });
+
+  
+  
+
+
